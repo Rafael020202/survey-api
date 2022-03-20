794551ddbddb28f642733ff48a683e3f2024c079
Rafael Luiz <luizrafael391@gmail.com>
2022-03-20 15:45:36 -0300

    feat: ensure SignUpController return 400 if no password_confirmation is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,7 @@ import { MissingParamError } from '../errors/missing-param-error';
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    const requiredParams = ['email', 'name', 'password'];
+    const requiredParams = ['email', 'name', 'password', 'password_confirmation'];
 
     for (const parm of requiredParams) {
       if (!httpRequest.body[parm]) return badRequest(new MissingParamError(parm));
