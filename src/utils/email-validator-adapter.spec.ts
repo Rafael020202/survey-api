4d623cc0eb08cc1fe7342cb410a52ee056fe227a
Rafael Luiz <luizrafael391@gmail.com>
2022-03-22 20:26:47 -0300

    feat: ensure false if isValid return false and true if isValid returns true

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,35 @@
+import { EmailValidatorAdapter } from './email-validator';
+import validator from 'validator';
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true;
+  }
+}));
+
+const makeEmailValidatorAdapter = (): EmailValidatorAdapter => {
+  const emailValidatorAdapter = new EmailValidatorAdapter();
+
+  return emailValidatorAdapter;
+};
+
+describe('EmailValidator Adapter', () => {
+  test('Should return false if isValid returns false', () => {
+    const emailValidatorAdapter = makeEmailValidatorAdapter();
+
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false);
+
+    const email = 'email@live.com';
+    const response = emailValidatorAdapter.isValid(email);
+
+    expect(response).toBe(false);
+  });
+
+  test('Should return true if isValid returns true', () => {
+    const emailValidatorAdapter = makeEmailValidatorAdapter();
+    const email = 'email@live.com';
+    const response = emailValidatorAdapter.isValid(email);
+
+    expect(response).toBe(true);
+  });
+});
