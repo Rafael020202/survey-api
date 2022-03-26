937dd872cf6ad17bce2e99c2788d25363bad9bf2
Rafael Luiz <luizrafael391@gmail.com>
2022-03-26 16:16:24 -0300

    feat: ensure AddAccountRepository is called with correct data

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../../domain/useCases/add-account';
 export * from '../../../domain/models/account';
 export { Encrypter } from '../../protocols/encrypter';
+export { AddAccountRepository } from '../../protocols/add-account-repository';
--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,12 +1,13 @@
-import { AccountModel, AddAccount, AddAccountModel, Encrypter } from './db-add-account-protocols';
+import { AccountModel, AddAccount, AddAccountModel, AddAccountRepository, Encrypter } from './db-add-account-protocols';
 
 export class DbAddAccount implements AddAccount {
   constructor (
-    private readonly encrypter: Encrypter
+    private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository
   ) {}
 
   async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password);
+    const hashedPassword = await this.encrypter.encrypt(account.password);
 
     const data: AccountModel = {
       created_at: new Date(),
@@ -16,6 +17,8 @@ export class DbAddAccount implements AddAccount {
       password: 'somepass',
       updated_at: new Date()
     };
+
+    await this.addAccountRepository.add(Object.assign({}, account, { password: hashedPassword }));
 
     return await new Promise((resolve) => resolve(data));
   }
