a482c2350f3c575576e0404462cccbe7ef070ec1
Rafael Luiz <luizrafael391@gmail.com>
2022-03-26 17:21:21 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,10 @@
 import { BCryptAdapter } from './bcrypt-adapter';
 import bcrypt from 'bcrypt';
 
+jest.mock('bcrypt', () => ({
+  hash: () => 'hashed_value'
+}));
+
 describe('BCrypt adapter', () => {
   test('Should call bcrypt with correct values', async () => {
     const sut = new BCryptAdapter(12);
@@ -10,5 +14,12 @@ describe('BCrypt adapter', () => {
     await sut.encrypt(value);
 
     expect(encrypt).toHaveBeenCalledWith(value, 12);
+  });
+
+  test('Should return a hash on success', async () => {
+    const sut = new BCryptAdapter(12);
+    const hash = await sut.encrypt('value');
+
+    expect(hash).toBe('hashed_value');
   });
 });
